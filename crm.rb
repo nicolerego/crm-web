aee4c97b4d7c0d195e99227f8314b4f8f47cea43
Nicole Rego <nicole.rego@live.com>
2015-01-27 12:48:39 -0500

    Adding new contacts route and view, creating form

--- crm.rb
+++ crm.rb
@@ -2,6 +2,8 @@ require_relative 'rolodex'
 require_relative 'contact'
 require 'sinatra'
 
+$rolodex= Rolodex.new
+
 get '/' do
 	@crm_app_name = "My CRM"
  	erb :index
@@ -9,14 +11,9 @@ end
 
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-	@contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-	@contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
- 	
  	erb :contacts
 end
 
 get '/contacts/new' do
- 	erb :contacts
+ 	erb :new_contact
 end
