2e940df84fd3f68b435b225b231755020c5f639a
Nicole Rego <nicole.rego@live.com>
2015-01-27 12:40:43 -0500

    Using Ruby to iterate over contacts from erb template

--- crm.rb
+++ crm.rb
@@ -8,6 +8,11 @@ end
 
 
 get '/contacts' do
+	@contacts = []
+	@contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+	@contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+	@contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+ 	
  	erb :contacts
 end
 
