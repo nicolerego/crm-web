f680cee7c0da3afe301ef7c798b7b9c79e58b9af
Nicole Rego <nicole.rego@live.com>
2015-01-28 12:26:50 -0500

    Adding views/layout.erb with a header, nav and footer

--- crm.rb
+++ crm.rb
@@ -11,14 +11,17 @@ end
 
 
 get '/contacts' do
+	@crm_app_name = "My CRM"
  	erb :contacts
 end
 
 get '/contacts/new' do
+	@crm_app_name = "My CRM"
  	erb :new_contact
 end
 
 post '/contacts' do
+	@crm_app_name = "My CRM"
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
