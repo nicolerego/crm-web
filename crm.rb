4216616c9e647d42900b0bdb6d2fedbd7f86fbe0
Nicole Rego <nicole.rego@live.com>
2015-01-29 12:35:42 -0500

    Adding edit_contact view, creating route

--- crm.rb
+++ crm.rb
@@ -24,15 +24,25 @@ end
 
 get "/contacts/:id" do
   @contact = $rolodex.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+  	erb :show_contact
   else
     raise Sinatra::NotFound
   end
 end
 
 post '/contacts' do
-	@crm_app_name = "My CRM"
+  @crm_app_name = "My CRM"
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
+
+get "/contacts/:id/edit" do 
+	@contact = $rolodex.find(params[:id].to_i)
+	if @contact
+		erb :edit_contact
+	else
+		raise Sinatra::NotFound
+	end
+end 
